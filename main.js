848ebc5425b742612fbbed87bf5fa527f678de4b
evyatar shlar <evyatar.szlar@gmail.com>
2024-12-24 20:29:46 +0200

    Edit onclick fun to rand

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var ballSize = 100
 
 function onBallClick(){
-    ballSize = ballSize +50 
+    ballSize = ballSize + getRandomInt(20,60) 
     if (ballSize > 400) ballSize = 100
     var elBall = document.querySelector('.ball')
     elBall.style.width = ballSize+ 'px'
