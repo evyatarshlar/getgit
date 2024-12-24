971261b7c915259b8c7b42c5a5c4288a164a4bb2
evyatar shlar <evyatar.szlar@gmail.com>
2024-12-24 19:59:01 +0200

    Limit the ball size

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 'use strict'
+var ballSize = 100
 
 function onBallClick(){
+    ballSize = ballSize +50 
+    if (ballSize > 400) ballSize = 100
     var elBall = document.querySelector('.ball')
-    elBall.style.width = '150px'
-    elBall.style.height = '150px'
-    elBall.innerHTML = '150px'
+    elBall.style.width = ballSize+ 'px'
+    elBall.style.height = ballSize+ 'px'
+    elBall.innerHTML = ballSize
 }
