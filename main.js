ec0bc695b3a349d2b365b783d988f9b875faea8d
evyatar shlar <evyatar.szlar@gmail.com>
2024-12-24 21:10:23 +0200

    Limit the balls sizes

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 'use strict'
 var ballSize = 100
 
-function onBallClick(){
+function onBallClick(elBall,maxDiameter){
     ballSize = ballSize + getRandomInt(20,60) 
-    if (ballSize > 400) ballSize = 100
-    var elBall = document.querySelector('.ball')
+    if (ballSize > maxDiameter) ballSize = 100
     elBall.style.width = ballSize+ 'px'
     elBall.style.height = ballSize+ 'px'
     elBall.innerHTML = ballSize
